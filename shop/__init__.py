75f18b9d3092830e4ae9bd0651bd83cb9b752736
Michaël <michael.gainyo@gmail.com>
2022-01-17 18:01:46 -0500

    new build

--- shop/__init__.py
+++ shop/__init__.py
@@ -5,5 +5,5 @@ SHOP APP
 
 __author__ = "Michaël Gainyo"
 __email__ = "michaelgainyo@gmail.com"
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 __copyright__ = "Copyright (C) 2021 Michaël Gainyo and contributors"
