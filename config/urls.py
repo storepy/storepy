b0be2482be5d6767113bec86e83e779e593e02bf
Michaël <michael.gainyo@gmail.com>
2022-07-04 20:29:35 -0400

    robots.txt

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.urls import path, include
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 from shopy.shop import views
 
@@ -24,6 +25,12 @@ urlpatterns += [
     path('', include('miq.staff.urls', namespace='staff')),
     path('', include('miq.core.urls', namespace='miq')),
     path('', views.IndexView.as_view(), name='index'),
+]
+
+urlpatterns += [
+    path('robots.txt', TemplateView.as_view(
+        template_name="core/robots.txt",
+        content_type="text/plain"), name="robots_file"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
