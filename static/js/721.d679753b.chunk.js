e986b3d49ef3224359beed7223415cabbd882996
Michaël <michael.gainyo@gmail.com>
2022-10-24 17:34:15 +0100

    new build

---
+++ static/js/721.d679753b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunk_storepy_clientjs=self.webpackChunk_storepy_clientjs||[]).push([[721],{1721:function(e,n,t){t.r(n),t.d(n,{default:function(){return l}});var r=t(7406),s=t(9996),u=t(7457),i=r.lazy((function(){return t.e(700).then(t.bind(t,700))})),c=r.lazy((function(){return t.e(165).then(t.bind(t,1165))}));function l(){return(0,u.jsxs)(s.Z5,{children:[(0,u.jsx)(s.AW,{path:":orderSlug/",element:(0,u.jsx)(i,{})}),(0,u.jsx)(s.AW,{index:!0,element:(0,u.jsx)(c,{})})]})}}}]);
+//# sourceMappingURL=721.d679753b.chunk.js.map
