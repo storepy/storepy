828cfec1676f796085209bb70300efffeac5a311
Michaël <michael.gainyo@gmail.com>
2022-10-27 09:49:54 +0100

    added beacon

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
 
 import './index.scss';
 
@@ -8,6 +8,7 @@ import { Loading } from '@miq/componentjs';
 import PartnerRoutes from './miqpartnersjs';
 import { SharedDataProvider } from '@miq/contextjs';
 import { LIBCreateView } from '@shopy/shopjs';
+import { DOMAIN } from '@miq/utiljs';
 
 const StaffLayout = React.lazy(() => import('./staff'));
 const ShopPublicRoutes = React.lazy(() => import('./shop'));
@@ -37,11 +38,36 @@ const App = () => {
 
 const root = document.getElementById('root');
 
+const Beacon = () => {
+  const { key } = useLocation();
+
+  React.useEffect(() => {
+    const cookies = document.cookie;
+    if (!cookies || cookies === '') return;
+
+    let token = cookies.split(';').find((i) => i.includes('csrftoken'));
+    if (token) token = token.split('=')[1];
+    if (!token || token === '') return;
+
+    const data = new FormData();
+    data.append('csrfmiddlewaretoken', token);
+
+    // console.log(navigator);
+
+    // data.append('ua', navigator.userAgent);
+    // data.append('url', window.location.href);
+
+    navigator.sendBeacon(`${DOMAIN}/beat/`, data);
+  }, [key]);
+  return null;
+};
+
 if (root)
   render(
     <React.StrictMode>
       <BrowserRouter>
         <App />
+        <Beacon />
       </BrowserRouter>
     </React.StrictMode>,
     root
--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,78 @@
+import random
+import hashlib
+import requests
+
 
 from django.contrib import admin
 from django.conf import settings
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
+from django.http import JsonResponse
 
 from shopy.shop import views
+from miq.analytics.models import Visitor
+from miq.analytics.utils import get_ip
+
+cache = {}
+
+
+def get_vid(ip, ua):
+    return hashlib.shake_128(bytes(f'{ip}{ua}', encoding='utf-8')).hexdigest(4)
+
+
+def set_cache_key(key, size=99):
+    keys = cache.keys()
+    if(len(keys) > size):
+        cache.pop(random.choice(keys, None))
+
+    cache[key] = True
+
+
+def beacon_view(request):
+    ua = request.META.get('HTTP_USER_AGENT')
+    ip = get_ip(request)
+    response = JsonResponse({'status': 'ok'})
+
+    key = get_vid(ip, ua)
+    if key in cache:
+        return JsonResponse({'status': 'ok'})
+
+    set_cache_key(key)
+
+    visitor = Visitor.objects.filter(ip=ip, user_agent=ua).order_by('created')
+    if not visitor.exists():
+        return response
+
+    visitor = visitor.first()
+    if visitor.is_parsed:
+        return response
+
+    # r = requests.get('https://api.ipgeolocation.io/ipgeo?apiKey=API_KEY&ip={ip}')
+
+    fields = 'as,asname,org,country,countryCode,region,regionName,currency,timezone,city,zip,status,message,isp,proxy,mobile'
+
+    r = requests.get(f'http://ip-api.com/json/{ip}', params={'fields': fields})
+    if r.status_code != 200 or not r.json().get('status') != 'success':
+        return response
+
+    data = r.json()
+    field_names = [f.name for f in visitor._meta.local_fields]
+    for k, v in data.items():
+        if not v or k not in field_names:
+            continue
+        setattr(visitor, k, v)
+
+    visitor.asfullname = data.get('as')
+    visitor.is_parsed = True
+    visitor.save()
+
+    # for key, value in request.POST.items():
+    #     if key == 'csrfmiddlewaretoken':
+    #         continue
+
+    return response
+
 
 urlpatterns = [
     path('miq/', admin.site.urls),
@@ -27,6 +94,8 @@ urlpatterns += [
     # !! careful when moving this !!
     path('', include('miq.honeypot.urls', namespace='honeypot')),
     # !!
+
+    path('beat/', beacon_view, name='beat'),
 
     path('', include('miq.core.urls', namespace='miq')),
     path('', views.IndexView.as_view(), name='index'),
